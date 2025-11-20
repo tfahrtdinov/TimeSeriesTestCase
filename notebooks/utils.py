afb261cd5c527d2f162931e5dc1359f60b0fb0ca
tfahrtdinov <fahrtdinov.ta@gmail.com>
2025-11-20 19:36:51 +0100

    Granger causality

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,7 +3,7 @@ from typing import cast
 import pandas as pd
 import numpy as np
 from numpy.typing import NDArray
-from statsmodels.tsa.stattools import adfuller, kpss, ccf
+from statsmodels.tsa.stattools import adfuller, kpss, ccf, grangercausalitytests
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
 
@@ -45,7 +45,57 @@ def ccf_two_direct(
     Returns:
          ccf
     """
-    c_xy = ccf(predictor, target, adjusted=True, nlags=nlags)
-    c_yx = ccf(target, predictor, adjusted=True, nlags=nlags)[:0:-1]
+    c_xy = ccf(target, predictor, adjusted=True, nlags=nlags)
+    c_yx = ccf(predictor, target, adjusted=True, nlags=nlags)[:0:-1]
 
     return np.concatenate((c_yx, c_xy))
+
+
+def granger_test(
+    df: pd.DataFrame,
+    target_col: str,
+    predictor_col: str,
+    maxlag: int = 10,
+    corr_threshold: float = 0.8,
+    significance_level: float = 0.05,
+) -> NDArray[np.float64]:
+    """
+    Run a filtered Granger causality test between two time series.
+
+    This function first computes the cross-correlation between the target and
+    predictor and selects only those lags whose absolute correlation exceeds
+    `corr_threshold`. For each selected lag, a Granger causality test is run.
+
+    Args:
+        df: DataFrame containing both time series.
+        target_col: Column name of the target series (Y).
+        predictor_col: Column name of the predictor series (X).
+        maxlag: Maximum lag to consider for the cross-correlation and Granger tests.
+        corr_threshold: Minimum absolute cross-correlation needed for a lag to be tested.
+        significance_level: The p-value threshold for rejecting the null hypothesis.
+
+    Returns:
+        ndarray of lags for which the null hypothesis of the Granger test
+        is rejected. Rejection means that at least one coefficient for the
+        predictor's lagged values is significantly different from zero, implying
+        that the predictor Granger-causes the target at that lag.
+    """
+    ccfs_mask = (
+        np.abs(ccf(df[target_col], df[predictor_col], nlags=maxlag + 1)[1:])
+        > corr_threshold
+    )
+    if ccfs_mask.any():
+        res_dict = grangercausalitytests(
+            df[[target_col, predictor_col]],
+            maxlag=np.arange(1, maxlag + 1)[ccfs_mask],
+            verbose=False,
+        )
+        return np.array(
+            [
+                k
+                for k, v in res_dict.items()
+                if v[0]["ssr_ftest"][1] < significance_level
+            ]
+        )
+    else:
+        return np.array([])
